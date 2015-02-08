d6246ecb2a662d4ce8f841dc1c1fcb47721abcf2
DRvanR <daan.vanrenterghem@gmail.com>
2015-02-08 19:02:41 +0100

    Updated design, added syntax highlighting

--- public/app.lua
+++ public/app.lua
@@ -26,19 +26,19 @@ ngx.say([[
 </head>
 <body>
     <header>
-        <div class="top-nav top-bar">
-        </div>
         <nav class="top-nav">
             <div class="container">
                 <div class="nav-wrapper">
-                    <span class="header-title">Current Page</span>
+                    <span class="header-title">Hello World!</span>
                     <a class="button-collapse" href="#" data-activates="nav-mobile">
                         <i class="mdi-navigation-menu"></i>
                     </a>
                 </div>
             </div>
             <ul id="nav-mobile" class="side-nav fixed">
-                <li class="logo">DRvanR</li>
+                <li class="logo center-align"><a href="/">DRvanR<span class="muted">.com</span></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"><i class="mdi-social-person left"></i>Whoami</a></li>
                 <li>
                     <a href="#">
                         <i class="mdi-communication-email left"></i>
@@ -51,25 +51,41 @@ ngx.say([[
     <main>
         <div class="container">
             <div class="row">
-                <div class="col s12 m10 l10 offset-m1 offset-l1">
-                    <div class="card white">
-                        <div class="card-content">
-                            <span class="card-title">Lipsum</span>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod consectetur est, vel vehicula nisl sodales sit amet. Donec luctus pharetra lacus vel rhoncus. Praesent tempus aliquam finibus. Donec lorem nibh, efficitur eget ante vitae, feugiat consectetur neque. Aenean at turpis eros. Donec eu nibh auctor, interdum metus in, sodales tortor. In eget turpis eu arcu interdum dictum non vitae dui. Duis eu lorem at tortor congue ullamcorper vestibulum eu magna. Vestibulum sagittis cursus molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                            </p>
-                            <p>
-                                Sed feugiat nulla dolor, nec viverra felis ultrices quis. Nam euismod libero non nisi finibus fringilla. Praesent tincidunt facilisis lectus, imperdiet sollicitudin risus ultricies sed. Nunc mollis fermentum felis, sed aliquam leo sollicitudin vel. Duis consequat dolor ac maximus venenatis. Phasellus imperdiet magna a diam convallis, quis ultricies justo mollis. Nulla nulla augue, vehicula ut convallis semper, bibendum a nibh. Integer blandit, nunc et gravida finibus, neque mi mollis mi, ut commodo magna nulla eu nisl. Nam pulvinar nulla in massa rhoncus, eget sagittis nibh ultricies. Curabitur porta gravida leo, vel commodo lacus gravida eget. Vestibulum scelerisque orci rutrum, laoreet est vel, mollis odio.
-                            </p>
-                            <p>
-                                Pellentesque sagittis enim dapibus, pharetra libero nec, lobortis purus. Phasellus venenatis nisi ac eros pretium euismod. Etiam vitae volutpat mi. Sed et eros quis nulla scelerisque egestas ac nec risus. Morbi sagittis tempor vulputate. In finibus vestibulum arcu, lobortis faucibus libero tristique at. Pellentesque hendrerit felis vel nisl pharetra, vitae sollicitudin augue rutrum. Aliquam erat volutpat. Suspendisse vel nibh ac ligula viverra vehicula. Nullam quis lectus turpis. Suspendisse vestibulum nunc non suscipit scelerisque. Nam bibendum turpis cursus, interdum erat ac, scelerisque dolor. Aenean nec mauris eu lorem iaculis accumsan. Curabitur porta lorem vel dignissim condimentum. Morbi ex augue, porta gravida leo id, feugiat ornare risus.
-                            </p>
-                        </div>
-                        <div class="card-action">
-                            <a href="#">This is a link</a>
-                            <a href='#'>This is a link</a>
-                        </div>
-                    </div>
+                <div class="col s12 m10 l10 offset-m1 offset-l1 content">
+                    <h1>Lispsum!</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod consectetur est, vel vehicula nisl sodales sit amet. Donec luctus pharetra lacus vel rhoncus. Praesent tempus aliquam finibus. Donec lorem nibh, efficitur eget ante vitae, feugiat consectetur neque. Aenean at turpis eros. Donec eu nibh auctor, interdum metus in, sodales tortor. In eget turpis eu arcu interdum dictum non vitae dui. Duis eu lorem at tortor congue ullamcorper vestibulum eu magna. Vestibulum sagittis cursus molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                    </p>
+                    <p>
+                        Sed feugiat nulla dolor, nec viverra felis ultrices quis. Nam euismod libero non nisi finibus fringilla. Praesent tincidunt facilisis lectus, imperdiet sollicitudin risus ultricies sed. Nunc mollis fermentum felis, sed aliquam leo sollicitudin vel. Duis consequat dolor ac maximus venenatis. Phasellus imperdiet magna a diam convallis, quis ultricies justo mollis. Nulla nulla augue, vehicula ut convallis semper, bibendum a nibh. Integer blandit, nunc et gravida finibus, neque mi mollis mi, ut commodo magna nulla eu nisl. Nam pulvinar nulla in massa rhoncus, eget sagittis nibh ultricies. Curabitur porta gravida leo, vel commodo lacus gravida eget. Vestibulum scelerisque orci rutrum, laoreet est vel, mollis odio.
+                    </p>
+                    <pre class="mono">
+                        <code class="lua">
+local location = {
+    foo = "/usr/local/lua/lib/pack1_1.lua",
+    goo = "/usr/local/lua/lib/pack1_1.lua",
+    foo1 = "/usr/local/lua/lib/pack1_2.lua",
+    goo1 = "/usr/local/lua/lib/pack1_3.lua",
+}
+
+    pack1 = {}
+
+setmetatable(pack1, {__index = function (t, funcname)
+  local file = location[funcname]
+  if not file then
+    error("package pack1 does not define " .. funcname)
+  end
+  assert(loadfile(file))()     -- load and run definition
+  return t[funcname]           -- return the function
+end})
+
+return pack1
+                        </code> 
+                    </pre>
+                    <p>
+                        Pellentesque sagittis enim dapibus, pharetra libero nec, lobortis purus. Phasellus venenatis nisi ac eros pretium euismod. Etiam vitae volutpat mi. Sed et eros quis nulla scelerisque egestas ac nec risus. Morbi sagittis tempor vulputate. In finibus vestibulum arcu, lobortis faucibus libero tristique at. Pellentesque hendrerit felis vel nisl pharetra, vitae sollicitudin augue rutrum. Aliquam erat volutpat. Suspendisse vel nibh ac ligula viverra vehicula. Nullam quis lectus turpis. Suspendisse vestibulum nunc non suscipit scelerisque. Nam bibendum turpis cursus, interdum erat ac, scelerisque dolor. Aenean nec mauris eu lorem iaculis accumsan. Curabitur porta lorem vel dignissim condimentum. Morbi ex augue, porta gravida leo id, feugiat ornare risus.
+                    </p>
+                    <p><a href="#">This is a link</a></p>
                 </div>
             </div>
         </div>  
@@ -77,9 +93,13 @@ ngx.say([[
     <footer class="page-footer">
         <div class="container">
             <div class="row">
-                <div class="col s4 offset-s8">
-                    There'll be something with contact info here :)
-                    You know, mail addresses or something
+                <div class="col s4">
+                    Powered By:
+                    <ul>
+                        <li><a href="http://openresty.org/" target="_blank">OpenResty</a><li>
+                        <li><a href="http://materializecss.com/" target="_blank">Materialize CSS</a><li>
+                        <li><a href="https://highlightjs.org/" target="_blank">highlight.js</a><li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -92,6 +112,7 @@ ngx.say([[
     <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript" src="js/site.js"></script>  
+    <script type="text/javascript" src="js/highlight.pack.js"></script>  
 </body>
 </html>
 ]]);
--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+    // mobile menu
     $(".button-collapse").sideNav();
+
+    // syntax highlighting
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
 })

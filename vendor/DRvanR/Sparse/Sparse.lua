0774a58f27529bb87fa79d61fcb8bd432e4fecfd
DRvanR <daan.vanrenterghem@gmail.com>
2015-02-10 21:38:02 +0100

    First Attempt at loading template files

--- vendor/DRvanR/Sparse/Sparse.lua
+++ vendor/DRvanR/Sparse/Sparse.lua
@@ -1,18 +1,31 @@
 -- imports
+local Error = require "DRvanR.Sparse.Error"
+local gsub = string.gsub
+local format = string.format
 
 -- class definition
 local Sparse = { _VERSION = '0.1' }
 local class = { __index = Sparse }
 
--- local function definition
-local function _sum(a, b)
-    return a + b
+-- public API
+function Sparse:new(root)
+    assert(root, 'Root must be set in order to use Sparse')
+    local instance = {_root=root}
+    return setmetatable(instance, class)
 end
 
--- public API
-function Sparse:new(constructorArgs)
-    local instance = constructorArgs or {}
-    return setmetatable(instance, class)
+function Sparse:load(filepath)
+    local path = self._root .. gsub(filepath, ':', '/')
+    local file = open(path, "rb")
+
+    if not file then 
+        error(Error:new(format("Template not found: '%s'", path)))
+    end
+
+    local content = file:read "*a"
+    file:close()
+
+    return content
 end
 
 function Sparse:parse(someString)

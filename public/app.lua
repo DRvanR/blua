261e5f19db9a786159e0f667ea91ad0b79145450
DRvanR <daan.vanrenterghem@gmail.com>
2015-02-10 21:40:13 +0100

    IdunnowhatIwasthinking...

--- public/app.lua
+++ public/app.lua
@@ -2,7 +2,7 @@
 package.path = "/var/www/drvanr.test/vendor/?.lua;" .. package.path
 
 local Sparse = require "DRvanR.Sparse.Sparse"
-local Templating = Sparse:new(root='/var/www/drvanr.test/src/')
+local Templating = Sparse:new('/var/www/drvanr.test/src/')
 
 ngx.header['X-Are-We-Happy'] = Templating:parse('why yes, yes we are!');
 ngx.say(Templating:load('Blog:Resources:View:index.html'));

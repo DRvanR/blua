c8ffc0c6c2cc321637ac09ebc5f130a239547a7a
DRvanR <daan.vanrenterghem@gmail.com>
2015-02-10 22:17:33 +0100

    Would be nice to actually do something..

--- public/app.lua
+++ public/app.lua
@@ -1,2 +1,7 @@
 -- need to work on this...
 package.path = "/var/www/drvanr.test/vendor/?.lua;" .. package.path
+
+local AppKernel = require "DRvanR.Homer.AppKernel"
+local kernel = AppKernel:new()
+
+kernel:handle()
